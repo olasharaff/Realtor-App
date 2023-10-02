820c971ac87a99fc691e816f43dfa3a3e9d86c02
AshraffDevv <sharafabdulrahman@yahoo.com>
2023-10-02 12:01:51 +0100

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Offer from './pages/Offer';
+import Profile from './pages/Profile';
+import ForgetPassword from './pages/ForgetPassword';
 
 
 function App() {
   return (
-   <>
-    Hello World!!!
-   </>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+          <Route path="/offer" element={<Offer />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
