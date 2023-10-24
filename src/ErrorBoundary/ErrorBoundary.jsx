b380b4a6b02b28e02f72f862474fcbcabdb6b786
AshraffDevv <sharafabdulrahman@icloud.com>
2023-10-24 16:33:43 +0100

    Create ErrorBoundary component and wrapped inside App.js

---
+++ src/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import { toast } from "react-toastify";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error caught by error boundary:", error, errorInfo);
+    // i use toastify to show error
+    toast.error("Error caught by error boundary");
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
